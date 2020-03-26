8b0ee5533dcfca9a9b0c3a2d66037a3a8da64443
Kyle Crockett <kyle@kyle-crockett.org>
2020-03-26 14:50:55 -0700

    Import data into actors table

--- hw1/sql/data-init.sql
+++ hw1/sql/data-init.sql
@@ -8,3 +8,11 @@ ALTER TABLE public.movie_data_temp ADD COLUMN id SERIAL PRIMARY KEY;
 
 
 -- TODO move data fromt emp table to ERD tables.
+ 
+
+-- Giving benifit of the doubt to the number of likes 
+INSERT INTO public.actors (name, facebook_likes) SELECT actor_1_name, MAX(actor_1_facebook_likes) FROM public.movie_data_temp WHERE actor_1_name IS NOT NULL GROUP BY actor_1_name;
+
+INSERT INTO public.actors (name, facebook_likes) SELECT actor_2_name, MAX(actor_2_facebook_likes) FROM public.movie_data_temp WHERE actor_2_name IS NOT NULL GROUP BY actor_2_name;
+
+INSERT INTO public.actors (name, facebook_likes) SELECT actor_3_name, MAX(actor_3_facebook_likes) FROM public.movie_data_temp WHERE actor_3_name IS NOT NULL GROUP BY actor_3_name;
