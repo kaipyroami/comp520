85ff93565900c8faa82fbdb8fcea0ba68cd80ddf
Kyle Crockett <kyle@kyle-crockett.org>
2020-03-26 18:59:37 -0700

    Add colors, countries and rating

--- hw1/sql/data-init.sql
+++ hw1/sql/data-init.sql
@@ -10,7 +10,7 @@ ALTER TABLE public.movie_data_temp ADD COLUMN id SERIAL PRIMARY KEY;
 -- TODO move data fromt emp table to ERD tables.
  
 
--- Giving benifit of the doubt to the number of likes 
+-- Giving benifit of the doubt to the number of likes. 
 -- Import Actors:
 INSERT INTO public.actors (name, facebook_likes) SELECT actor_1_name, MAX(actor_1_facebook_likes) FROM public.movie_data_temp WHERE actor_1_name IS NOT NULL GROUP BY actor_1_name;
 
@@ -20,3 +20,15 @@ INSERT INTO public.actors (name, facebook_likes) SELECT actor_3_name, MAX(actor_
 
 -- Import Directors:
 INSERT INTO public.directors (name, facebook_likes) SELECT director_name, MAX(director_facebook_likes) FROM public.movie_data_temp WHERE director_name IS NOT NULL GROUP BY director_name;
+
+-- Import Colors:
+INSERT INTO public.colors (color) SELECT color FROM public.movie_data_temp WHERE color IS NOT NULL GROUP BY color;
+
+-- Import Countries:
+INSERT INTO public.countries (country) SELECT country FROM public.movie_data_temp WHERE country IS NOT NULL GROUP BY country;
+
+-- Cleaning Data.
+-- Import Content Ratings:
+INSERT INTO public.content_ratings (content_rating) SELECT content_rating FROM public.movie_data_temp WHERE content_rating IS NOT NULL GROUP BY content_rating;
+UPDATE public.content_ratings content_rating SET content_rating = 'Not Rated' WHERE content_rating = 'Unrated';
+DELETE FROM public.content_ratings WHERE content_rating = 'Unrated';
